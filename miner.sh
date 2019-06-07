2597040f4c8a62734d13be9f6178d9402e109fcd
Mirk <mirko.lance1@gmail.com>
2019-06-07 17:33:22 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,5 +2,5 @@
 echo "Praying... Press [CTRL+C] to stop"
 while :
 do
-  nobilitas-cli generatetoaddress 1 2MxZu5UrybtQo9kmfb9nZYzUezAnJ42cbXo
+  nobilitas-cli $1 generatetoaddress 1 2MxZu5UrybtQo9kmfb9nZYzUezAnJ42cbXo
 done
